74d82c42e20f741340bcfe6887f2cf484b2da9d0
dev1 <dev1@gmail.com>
2018-06-10 14:04:06 +0530

    first chnage

--- GitHub/src/com/nt/vishnu/SamDemo.java
+++ GitHub/src/com/nt/vishnu/SamDemo.java
@@ -3,6 +3,10 @@ package com.nt.vishnu;
 public class SamDemo {
 	public static void main(String[] args) {
 		System.out.println("hello");
+
+	for(int i=1;i<=10;i++){
+		System.out.println(i);
+	}
 	}
 
 }
